b5b5310f95cf8bc7baf38f49c57fc7d4d3107c8e
Parker Smith <parker@parkersmith.us>
2015-05-24 01:45:24 -0600

    Add namespaces

--- spec/AbodeSpec.php
+++ spec/AbodeSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec;
+namespace spec\Abode;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -9,6 +9,6 @@ class AbodeSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Abode');
+        $this->shouldHaveType('Abode\Abode');
     }
 }
--- src/Abode.php
+++ src/Abode.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Abode;
+
 class Abode
 {
 }

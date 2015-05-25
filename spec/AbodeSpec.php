4c4af7a289099f1966184b13ff09789c26702ca2
Parker Smith <parker@parkersmith.us>
2015-05-24 22:15:06 -0600

    Implement validation and error handling

--- spec/AbodeSpec.php
+++ spec/AbodeSpec.php
@@ -4,9 +4,17 @@ namespace spec\Abode;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Abode\TenantHandler;
 
 class AbodeSpec extends ObjectBehavior
 {
+	function let(HttpKernelInterface $app, TenantHandler $handler)
+	{
+		$this->beConstructedWith($app, $handler);
+	}
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Abode\Abode');
@@ -15,5 +23,29 @@ class AbodeSpec extends ObjectBehavior
     function it_extends_HttpKernelInterface()
     {
     	$this->shouldHaveType('Symfony\Component\HttpKernel\HttpKernelInterface');
+    }
+
+    function it_calls_handle_on_next_layer_when_validation_passes(HttpKernelInterface $app, Request $request, TenantHandler $handler)
+    {
+    	$this->beConstructedWith($app, $handler);
+
+    	$handler->validate($request)->willReturn(true);
+
+    	$app->handle($request)->shouldBeCalled();
+
+    	$this->handle($request);
+    }
+
+    function it_calls_failed_on_handler_when_validation_fails(HttpKernelInterface $app, Request $request, TenantHandler $handler)
+    {
+    	$this->beConstructedWith($app, $handler);
+
+    	$handler->validate($request)->willReturn(false);
+
+    	$app->handle($request)->shouldNotBeCalled();
+
+    	$handler->failed()->shouldBeCalled();
+
+    	$this->handle($request);
     }
 }
--- src/Abode.php
+++ src/Abode.php
@@ -7,8 +7,24 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Abode implements HttpKernelInterface
 {
+	protected $app;
+
+	protected $handler;
+
+	public function __construct(HttpKernelInterface $app, TenantHandler $handler)
+	{
+		$this->app = $app;
+
+		$this->handler = $handler;
+	}
+
 	public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
 	{
+		if ( $this->handler->validate($request))
+		{
+			return $this->app->handle($request);
+		}
 
+		return $this->handler->failed();
 	}
 }
